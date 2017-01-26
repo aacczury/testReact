8f0c27a8f265832c529894266aae4c51cb4d359d
deleav <a42006310@gmail.com>
2017-01-26 12:23:57 +0800

    Updates

---
+++ config.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+
+var config = {
+  apiKey: "AIzaSyAjdarRIsUshhdQOwaFLxlBneFWz12JrTU",
+  authDomain: "chcwcup.firebaseapp.com",
+  databaseURL: "https://chcwcup.firebaseio.com",
+};
+firebase.initializeApp(config);
