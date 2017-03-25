468a65f415f0df02eb3bb383ecc4445086d8b14b
aacczury <yl931905@gmail.com>
2017-03-25 15:06:46 +0800

    can send mail

--- public/mail.asp
+++ public/mail.asp
@@ -1,9 +1,10 @@
+<% @codepage=65001 %>
 <%
 Set myMail = CreateObject("CDO.Message")
 myMail.Subject = Request.Form("title")
 myMail.From = "chcwcup@mail.ncku.edu.tw"
-myMail.To = "yl931905@gmail.com"
-myMail.CreateMHTMLBody = "http://stud.adm.ncku.edu.tw/act/chcwcup/register/?th=11&university=ncku&sport=-Kfvb5YCBPyMLRWOk-zt"
+myMail.To = Request.Form("email")
+myMail.HTMLBody = Request.Form("body")
 myMail.Send
 set myMail = nothing
 %>
--- src/pages/Participants.js
+++ src/pages/Participants.js
@@ -4,9 +4,9 @@ import {Card, CardTitle, CardText, IconButton, RaisedButton} from 'material-ui';
 
 import ParticipantInfo from '../components/ParticipantInfo';
 import Input from '../components/Input';
-import '../components/ResTable.css';
+import AddDialog from '../components/AddDialog';
 
-import iconv from 'iconv-lite';
+import '../components/ResTable.css';
 
 class Participants extends Component {
   constructor(props) {
@@ -14,13 +14,14 @@ class Participants extends Component {
 
     this.state = {
       tableData: [],
-      participantsData: {},
+      participantsData: [],
       contact: {
         name: "",
         phone: "",
         email: ""
       },
-      sportData: {}
+      sportData: {},
+      sendEmailDialogOpen: false
     };
     this.tmpUpload = {};
     this.uploadTimer = null;
@@ -73,11 +74,15 @@ class Participants extends Component {
     // need loading icon
     let data = d ? d : {};
     let sportData = s ? s : {};
-    let tableData = [];
+    let tableData = [], participantsData = [];
     statusList.map((status, index) => {
       if(status !== "member" && status in data && data[status]) {
         tableData.push(<ParticipantInfo key={`ptc_high_${index}`} user={this.props.user}
           university={this.props.university} th={this.props.th} uid={data[status]} status={statusName[status]} />)
+        participantsData.push({
+          uid: data[status],
+          status: statusName[status]
+        })
       }
       return 0;
     });
@@ -92,12 +97,17 @@ class Participants extends Component {
         else
           tableData.push(<ParticipantInfo key={`ptc_member_${index}`} user={this.props.user} university={this.props.university} th={this.props.th} uid={uid}
             status={memberName} />);
+        participantsData.push({
+          uid: uid,
+          status: memberName
+        })
         return 0;
       });
     }
 
     this.setState({ // need loading
       tableData: tableData,
+      participantsData: participantsData,
       contact: data.contact,
       sportData: sportData
     });
@@ -157,15 +167,119 @@ class Participants extends Component {
     this.uploadTimer = setTimeout(() => this.uploadContact(this.tmpUpload), 3000);
   }
 
+  getParticipantData = (data) => {
+    const keyList = ["name", "deptyear", "id", "birthday", "size", "lodging", "bus", "vegetarian"];
+    let d = data ? data : {};
+
+    keyList.map(k => {
+      if(typeof d[k] === "undefined") {
+        if(k === "lodging" || k === "bus" || k === "vegetarian") d[k] = false;
+        else d[k] = "";
+      }
+      return 0;
+    });
+
+    if(d.birthday !== "") {
+      d.birthday = new Date(d.birthday);
+      d.birthday = `${d.birthday.getFullYear()}-${d.birthday.getMonth()}-${d.birthday.getDate()}`;
+    }
+
+    return {
+      id: d.id,
+      name: d.name,
+      deptyear: d.deptyear,
+      birthday: d.birthday,
+      size: String(d.size).toUpperCase(),
+      lodging: d.lodging ? 'V' : '',
+      bus: d.bus ? 'V' : '',
+      vegetarian: d.vegetarian ? 'V' : ''
+    }
+  }
+
   handleSendEmail = () => {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "http://stud.adm.ncku.edu.tw/act/chcwcup/register/mail.asp", true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    let title = iconv.encode(`[第${this.props.th}屆正興城灣盃] ${this.state.sportData.title} 報名資料`, "big5");
-    let body = iconv.encode(`<table><tr><td></td><td>姓名</td><td>電話</td><td>信箱</td></tr>${this.state.contact.name}`, "big5")
-    xhr.send(`uid=${this.props.user.uid}&email=${this.state.contact.email}&title=${title}&body=${body}`);
-    //xhr.send();
+    let title = encodeURI(`[第${this.props.th}屆正興城灣盃]+${this.state.sportData.title}+報名資料`);
+    let body = `
+    <div style="font-family:sans-serif,微軟正黑體;">
+      ${this.state.contact.name} 您好：<br /><br />
+      歡迎使用第${this.props.th}屆正興城灣盃報名系統，<br />
+      您被設定為<span style="color:#2196f3">${this.state.sportData.title}</span>的聯絡人，<br />
+      <span style="color:#2196f3">${this.state.sportData.title}</span>的報名資料如下：<br /><br />`;
+    body += `
+      <table style='font-family:sans-serif,微軟正黑體;border-collapse:collapse;border:1px solid #888;'>
+        <tr style='border:1px solid #888;font-weight:900px;'>
+          <th style='font-family:sans-serif,微軟正黑體;padding:5px;color:#fff;background-color:#00bcd4;'></th>
+          <th style='font-family:sans-serif,微軟正黑體;padding:5px;color:#fff;background-color:#00bcd4;'>姓名</th>
+          <th style='font-family:sans-serif,微軟正黑體;padding:5px;color:#fff;background-color:#00bcd4;'>電話</th>
+          <th style='font-family:sans-serif,微軟正黑體;padding:5px;color:#fff;background-color:#00bcd4;'>信箱</th>
+        </tr>
+        <tr style='border:1px solid #888;'>
+          <td style='font-family:sans-serif,微軟正黑體;padding:5px;'>聯絡人</td>
+          <td style='font-family:sans-serif,微軟正黑體;padding:5px;'>${this.state.contact.name}</td>
+          <td style='font-family:sans-serif,微軟正黑體;padding:5px;'>${this.state.contact.phone}</td>
+          <td style='font-family:sans-serif,微軟正黑體;padding:5px;'>${this.state.contact.email}</td>
+        </tr>
+      </table><br /><br />`;
+
+    const attrList = ["status", "name", "deptyear", "id", "birthday", "size", "lodging", "bus", "vegetarian"];
+    const attrName = {
+      status: "身分", name: "姓名", deptyear: "系級", id: "身分證字號",
+      birthday: "出生年月日", size: "衣服尺寸", lodging: "住宿", bus: "搭乘遊覽車", vegetarian: "素食"}
+
+    body += `
+      <table style='font-family:sans-serif,微軟正黑體;border-collapse:collapse;border:1px solid #888;'>
+        <tr style='border:1px solid #888;font-weight:900px;'>`;
+    attrList.map(attr => {
+      body += `<th style='font-family:sans-serif,微軟正黑體;padding:5px;color:#fff;background-color:#00bcd4;'>${attrName[attr]}</th>`;
+      return 0;
+    })
+    body += `</tr>`;
+
+    for(let i = 0; i < this.state.participantsData.length; ++i) {
+      let ptc = this.state.participantsData[i];
+      let self = this;
+      window.firebase.database().ref(`/participant/${this.props.university}/${this.props.th}/${ptc.uid}`).once('value').then(ptcInfoShot => {
+        let ptcInfo = self.getParticipantData(ptcInfoShot.val() ? ptcInfoShot.val() : {});
+        body += `<tr style='border:1px solid #888;'>
+                  <td style='font-family:sans-serif,微軟正黑體;padding:5px;'>${ptc.status}</td>`;
+        attrList.map(attr => {
+          if(attr !== "status") {
+            body += `<td style='font-family:sans-serif,微軟正黑體;padding:5px;'>${ptcInfo[attr]}</td>`;
+          }
+          return 0;
+        })
+        body += `</tr>`;
+        if(i === this.state.participantsData.length - 1) {
+          body += `</table><br />`;
+          body += `因資料已送出，無法再於系統修改，<br />
+                    如仍有需修改的資料或任何報名上的疑問，<br />
+                    煩請聯絡本屆正興城灣盃負責人：<br />
+                    惠姿，09XXXXXXXX，XXX@XXX.XXX<br />
+                    <br />
+                    謝謝，<br />
+                    正興城灣盃籌備團隊敬上<br />
+                    </div>
+                    `
+
+          body = encodeURI(body);
+          xhr.send(`uid=${this.props.user.uid}&email=${this.state.contact.email}&title=${title}&body=${body}`);
+
+          self.handleSendEmailDialogClose();
+        }
+      }, err => err && console.log(err));
+    }
   }
+
+  handleSendEmailDialogOpen = () => {
+    this.setState({sendEmailDialogOpen: true})
+  }
+
+  handleSendEmailDialogClose = () => {
+    this.setState({sendEmailDialogOpen: false})
+  }
+
 
   render() {
     let cancelHeadCell = (<th></th>);
@@ -187,7 +301,7 @@ class Participants extends Component {
         <div style={{textAlign: "center"}}>
           <div><ActionHome /></div>
           <div>
-            <Card style={{margin: "10px", display: "inline-block", verticalAlign: "top"}}>
+            <Card style={{display: "inline-block"}}>
               <CardText>
                 <table>
                   <thead>
@@ -215,12 +329,6 @@ class Participants extends Component {
                     </tr>
                   </tbody>
                 </table>
-                <RaisedButton
-                  onTouchTap={this.handleSendEmail}
-                  label="寄出"
-                  secondary={true}
-                  style={{margin: "12px"}}
-                />
               </CardText>
             </Card>
           </div>
@@ -250,7 +358,21 @@ class Participants extends Component {
               </table>
             </CardText>
           </Card>
+
+          <div>
+            <RaisedButton
+              onTouchTap={this.handleSendEmailDialogOpen}
+              label="送出"
+              secondary={true}
+              style={{margin: "12px"}}
+            />
+          </div>
         </div>
+
+        <AddDialog title="確認送出" addDialogOpen={this.state.sendEmailDialogOpen} handleAddSubmit={this.handleSendEmail}
+          handleAddDialogOpen={this.handleSendEmailDialogOpen} handleAddDialogClose={this.handleSendEmailDialogClose}
+          content={<div>送出後將無法再做修改，<br />並會將此報名資料寄送給聯絡人。</div>} />
+
       </div>
     );
 
