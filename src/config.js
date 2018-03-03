55a893d22263d42535f9e2f0922ca0929659cb26
aacczury <yl931905@gmail.com>
2018-03-03 16:52:35 +0800

    feature: birthday no using data DatePicker

--- src/config.js
+++ src/config.js
@@ -8,9 +8,9 @@ const highStatusList = ["coach", "captain", "manager", "leader"];
 const statusName = {coach: "教練", captain: "領隊", manager: "管理", leader: "隊長", member: "隊員"};
 
 const attrList = ["name", "deptyear", "id", "birthday", "size", "lodging", "bus", "vegetarian"];
-const attrName = {name: "姓名", deptyear: "系級", id: "身分證字號", birthday: "出生年月日", size: "衣服尺寸",
+const attrName = {name: "姓名", deptyear: "系級", id: "身分證字號", birthday: "出生年月日  (例: 1991-01-01)", size: "衣服尺寸",
                     lodging: "住宿", bus: "搭乘遊覽車", vegetarian: "素食", status: "身分"};
-const attrType = {name: "text", deptyear: "text", id: "text", birthday: "date",
+const attrType = {name: "text", deptyear: "text", id: "text", birthday: "text",
                     size: "text", lodging: "checkbox", bus: "checkbox", vegetarian: "checkbox"}
 
 export {
